ec3df3b02f156d204f569f6e526f816b090a9927
cortinabryan <64225493+cortinabryan@users.noreply.github.com>
2023-03-27 14:33:37 -0700

    Modified client folder. added react with typesript

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+// const App: React.FC = () => {
+//   return <div className="App"></div>;
+// }
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
