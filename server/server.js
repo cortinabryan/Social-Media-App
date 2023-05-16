6c9dabd70a8ec141f888b863aefb7e69f29f3280
cortinabryan <64225493+cortinabryan@users.noreply.github.com>
2023-05-16 13:08:28 -0700

    installed more backend dependancies

--- server/server.js
+++ server/server.js
@@ -1,6 +1,17 @@
 // const Data = require("./Data");
 
-const express = require("express");
+// const express = require("express");
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+
 const app = express();
 const PORT = 5000;
 
@@ -11,3 +22,19 @@ app.get("/api", (req, res) => {
 app.listen(PORT, () =>
   console.log(`Server runing on port: http://localhost:${PORT}`)
 );
+
+/////////////////////////////////
+
+// Before Experimentation
+
+// import express from "express";
+// const app = express();
+// const PORT = 5000;
+
+// app.get("/api", (req, res) => {
+//   res.json({ users: ["userOne", "userTwo", "userThree", "userFour"] });
+// });
+
+// app.listen(PORT, () =>
+//   console.log(`Server runing on port: http://localhost:${PORT}`)
+// );
