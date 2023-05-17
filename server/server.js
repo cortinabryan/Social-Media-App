b8c4c7f6ee2e984264eec2d95fdba6dbc5cb93f5
cortinabryan <64225493+cortinabryan@users.noreply.github.com>
2023-05-17 14:55:38 -0700

    commented out dependancies

--- server/server.js
+++ server/server.js
@@ -2,15 +2,15 @@
 
 // const express = require("express");
 import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import cors from "cors";
-import dotenv from "dotenv";
-import multer from "multer";
-import helmet from "helmet";
-import morgan from "morgan";
-import path from "path";
-import { fileURLToPath } from "url";
+// import bodyParser from "body-parser";
+// import mongoose from "mongoose";
+// import cors from "cors";
+// import dotenv from "dotenv";
+// import multer from "multer";
+// import helmet from "helmet";
+// import morgan from "morgan";
+// import path from "path";
+// import { fileURLToPath } from "url";
 
 const app = express();
 const PORT = 5000;
